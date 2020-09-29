e81501cfbf0a12f25da985f91027880c21743e46
chusang23 <45816771+chusang23@users.noreply.github.com>
2020-09-29 23:47:14 +0900

    build router

--- src/routes/home.js
+++ src/routes/home.js
@@ -25,23 +25,23 @@ class Home extends React.Component {
     const { isLoading, movies } = this.state;
     return (
       <section className="container">
-      {isLoading ? (
-        <div className="loader">
-          <span className="loader__text">Loading...</span>
-        </div>
-      ) : (
-        <div className="movies">
-          {movies.map(movie => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-              genres={movie.genres}
-            />
-          ))}
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
           </div>
         )}
       </section>
@@ -49,4 +49,4 @@ class Home extends React.Component {
   }
 }
 
-export default App;
+export default Home;
